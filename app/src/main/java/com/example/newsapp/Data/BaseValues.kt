e5abffc45885042c77a6753bd66558348315ef61
Seth Osric Torralba <sethosrictorralba@gmail.com>
2024-08-22 15:53:37 -0500

    Added BaseValues Back
    Ignored ApiKey

---
+++ app/src/main/java/com/example/newsapp/Data/BaseValues.kt
@@ -0,0 +1,5 @@
+package com.example.newsapp.Data
+
+object BaseValues {
+    val baseURL = "https://api.thenewsapi.com/v1/news/"
+}
--- app/src/main/java/com/example/newsapp/Repo/MainRepo.kt
+++ app/src/main/java/com/example/newsapp/Repo/MainRepo.kt
@@ -1,5 +1,6 @@
 package com.example.newsapp.Repo
 
+import com.example.newsapp.Data.ApiKey
 import com.example.newsapp.Data.AuthInterceptor
 import com.example.newsapp.Data.BaseValues
 import com.example.newsapp.Data.MyAPI
@@ -11,7 +12,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 class MainRepo {
 
     private val okHttpClient = OkHttpClient.Builder()
-        .addInterceptor(AuthInterceptor(BaseValues.apiToken))
+        .addInterceptor(AuthInterceptor(ApiKey.apiToken))
         .build()
 
     private val api = Retrofit.Builder()
