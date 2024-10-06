fb31487c5d3fed91c5fbe1796d5aa2ffd440bfea
Seth Osric Torralba <sethosrictorralba@gmail.com>
2024-10-06 10:47:28 -0500

    Updated Playground Module

---
+++ Playground/src/main/java/com/example/playground/MyClass.kt
@@ -0,0 +1,9 @@
+/*
+ * Copyright (c) 2024. Seth Torralba
+ */
+
+package com.example.playground
+
+fun main() {
+    println("Hello World")
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,4 +21,4 @@ dependencyResolutionManagement {
 
 rootProject.name = "NewsApp"
 include(":app")
- 
+include(":Playground")
